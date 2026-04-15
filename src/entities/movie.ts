0de3949bf60c957bff3ba152827a963635cd0089
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-15 12:49:24 +0100

    Include extra information to movie entity

--- src/entities/movie.ts
+++ src/entities/movie.ts
@@ -8,6 +8,6 @@ export interface Movie {
     duration: number; // Duración en minutos
     poster: string; // URL de la película
     rate: number; // ratio de la película
-    genres?: Genre[]; // Array de los géneros
+    genres?: Genre[]; // Array de los id de los géneros asociados a las películas. Quizá no relevante para esta ocasión pero si para otras.
 
 }
