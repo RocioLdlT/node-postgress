80307473c00f60ed2bd01337b7f7d66ea12dc6ed
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-16 11:40:24 +0100

    Update repos genres and movies

--- src/repo/genres.ts
+++ src/repo/genres.ts
@@ -1,33 +1,84 @@
 import debug from 'debug';
 import { env } from '../config/env.ts';
 import type { Genre } from '../entities/genre.ts';
-import type { Pool } from "pg";
+import type { Pool } from 'pg';
+import { SqlError } from '../errors/sql-error.ts';
 
-const log = debug (`${env.PROJECT_NAME}:repo.genres`);
-log("Starting genres repository...");
+const log = debug(`${env.PROJECT_NAME}:repo.genres`);
+log('Starting genres repository...');
 
-export class GenresRepo{
-    private pool: Pool
-    constructor (pool: Pool){
+export class GenresRepo {
+    private pool: Pool;
+    constructor(pool: Pool) {
         this.pool = pool;
     }
 
-    async readAllGenres () {
-        const{rows} = await this.pool.query<Genre>('SELECT genre_id AS id, name FROM genres')
+    async readAllGenres() {
+        const { rows } = await this.pool.query<Genre>(
+            'SELECT genre_id AS id, name FROM genres',
+        );
         return rows;
     }
-    async readGenreById (id: number): Promise<Genre>{
-        const q =`
+    async readGenreById(id: number): Promise<Genre> {
+        const q = `
         SELECT genre_id AS id, name
         FROM genres
         WHERE genre_id = $1`;
-        const{rows} = await this.pool.query<Genre>(q, [id]);
+        const { rows } = await this.pool.query<Genre>(q, [id]);
 
-        if(rows.length === 0){
-            throw new Error (`Genre with id ${id} not found`
-            )
+        if (rows.length === 0) {
+            throw new SqlError(`Genre with id ${id} not found`, {
+                code: 'NOT_FOUND',
+                sqlState: 'READ_FAILED',
+                sqlMessage: `No genre found with id ${id}`,
+            });
         }
-    return rows[0] as Genre
+
+        return rows[0] as Genre;
+    }
+        async createGenre (name: string) {
+        const q = `
+            INSERT INTO genres (name) 
+            VALUES ($1) 
+            RETURNING genre_id AS id, name`;
+        const { rows } = await this.pool.query<Genre>(q, [name]);
+        return rows[0];
     }
 
+    async updateGenre (id: number, name: string) {
+        const q = `
+            UPDATE genres 
+            SET name = $2 
+            WHERE genre_id = $1 
+            RETURNING genre_id AS id, name`;
+        const { rows } = await this.pool.query<Genre>(q, [id, name]);
+
+        if (rows.length === 0) {
+            throw new SqlError(`Genre with id ${id} not found`, {
+                code: 'NOT_FOUND',
+                sqlState: 'UPDATE_FAILED',
+                sqlMessage: `No genre found with id ${id}`,
+            });
+        }
+
+        return rows[0] as Genre;
+    }
+
+    async deleteGenre (id: number) {
+        const q = `
+            DELETE FROM genres 
+            WHERE genre_id = $1 
+            RETURNING genre_id AS id, name`;
+        const { rows } = await this.pool.query<Genre>(q, [id]);
+
+        if (rows.length === 0) {
+            throw new SqlError(`Genre with id ${id} not found`, {
+                code: 'NOT_FOUND',
+                sqlState: 'DELETE_FAILED',
+                sqlMessage: `No genre found with id ${id}`,
+            });
+        }
+
+        return rows[0] as Genre;
+    }
 }
--- src/repo/movies.ts
+++ src/repo/movies.ts
@@ -6,13 +6,15 @@ import type { Pool } from 'pg';
 const log = debug(`${env.PROJECT_NAME}:repo.movies`);
 log('Starting movies repository...');
 
+type MovieWithInfo = Movie & { info_genres: string[] };
+
 export class MoviesRepo {
     private pool: Pool;
     constructor(pool: Pool) {
         this.pool = pool;
     }
 
-    async readAllMoviesWithGenres() {
+    async readAllMovies() {
         const q = `
             SELECT 
             movie_id as id,
@@ -26,4 +28,70 @@ export class MoviesRepo {
         const { rows } = await this.pool.query<Movie>(q);
         return rows[0];
     }
+    private obtainGenre(movie: MovieWithInfo): Movie {
+        if (movie.info_genres) {
+            movie.genres = movie.info_genres.map((g) => {
+                const [id, name] = g.split('|');
+                return { id: Number(id), name: name as string };
+            });
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { info_genres, ...rest } = movie as MovieWithInfo;
+        return rest;
+    }
+
+    async readAllMoviesWithGenres() {
+        const q = `
+           select mo.movie_id AS id, 
+                mo.title,
+                ARRAY_AGG(ge.genre_id || '|' || ge.name) as info_genres,
+                mo.release_year as year, 
+                mo.director, 
+                mo.duration, 
+                mo.poster, 
+                mo.rate
+            from movies mo
+            join movies_genres mg
+            on mo.movie_id = mg.movie_id
+            join genres ge
+            on ge.genre_id = mg.genre_id
+            group by mo.movie_id 
+            order by mo.movie_id;
+            `;
+        const { rows } = await this.pool.query<MovieWithInfo>(q);
+        const result = rows.map(this.obtainGenre);
+        return result as Movie[];
+    }
+
+    // readMoviesWithGenresById(id: number)
+
+    async findMoviesWithGenresByTitle(title: string) {
+        const q = `
+        select mo.movie_id AS id, 
+            mo.title,
+            ARRAY_AGG(ge.genre_id || '|' || ge.name) as info_genres,
+            mo.release_year as year, 
+            mo.director, 
+            mo.duration, 
+            mo.poster, 
+            mo.rate
+        from movies mo
+        join movies_genres mg
+        on mo.movie_id = mg.movie_id
+        join genres ge
+        on ge.genre_id = mg.genre_id
+        where mo.title ilike $1
+        group by mo.movie_id
+        order by mo.movie_id
+        `;
+
+        const { rows } = await this.pool.query<MovieWithInfo>(q, [
+            `%${title}%`,
+        ]);
+        const result = rows.map(this.obtainGenre);
+        return result as Movie[];
+    }
+
+    // createMovie(movie: Omit<Movie, 'id'>)
+
 }
