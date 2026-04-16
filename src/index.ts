8fc8d5c02af1991cfedb09b30f59b1547448bc62
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-16 12:17:05 +0100

    Add moviesRepo with genres to our index

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import debug from "debug";
-import{ env } from "./config/env.ts"
-import { connectDB } from "./config/db.ts"
-import { GenreRepo } from "./repo/genres.ts";
+import debug from 'debug';
+import { env } from './config/env.ts';
+import { connectDB } from './config/db.ts';
+import { GenresRepo } from './repo/genres.ts';
+import { MoviesRepo } from './repo/movies.ts';
 
-const log = debug (`${env.PROJECT_NAME}:index`);
-log("Starting application...");
+const log = debug(`${env.PROJECT_NAME}:index`);
+log('Starting application...');
 
 // Preparación para el genres.test.ts
 const pool = await connectDB();
-const genresRepo = new GenreRepo(pool);
+const genresRepo = new GenresRepo(pool);
+const moviesRepo = new MoviesRepo(pool);
 
 // Primer test
 const g = await genresRepo.readAllGenres();
-log('Genres:', g)
+log('Genres:', g);
 
 // Segundo test: leer un género por id
 try {
     const g2 = await genresRepo.readGenreById(100);
     log('Genre with id 100', g2);
-}catch (error) {
+} catch (error) {
     log((error as Error).message);
 }
 
-
-
+const movies = await moviesRepo.readAllMoviesWithGenres();
+log('Movies with genres:', movies[0]);
