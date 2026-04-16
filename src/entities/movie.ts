3a827b452e27a6310e942f01393c0c18a3467bac
RocioLdlT <r.delatorre.f95@gmail.com>
2026-04-16 13:59:17 +0100

    Add interface relations-movie-genre

--- src/entities/movie.ts
+++ src/entities/movie.ts
@@ -11,3 +11,9 @@ export interface Movie {
     genres?: Genre[]; // Array de los id de los géneros asociados a las películas. Quizá no relevante para esta ocasión pero si para otras.
 
 }
+
+ export interface RelationsMovieGenre{
+    movies_id: number;
+    genres_id: number; // es number porque es un id, string sería el name del genre
+
+ }
